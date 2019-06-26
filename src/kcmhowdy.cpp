73c52a17fc7bcce3865ffc709add0a1ded67fcfd
Petr Dušek <dusekpe2@fit.cvut.cz>
2019-06-26 20:13:08 +0200

    Update src/kcmhowdy.cpp

--- src/kcmhowdy.cpp
+++ src/kcmhowdy.cpp
@@ -12,6 +12,13 @@ K_PLUGIN_FACTORY(howdyConfigFactory, registerPlugin<KcmHowdy>();)
 
 using namespace std;
 
+/**
+ * KCM module construtor
+ * It gets name of actual user and sets configuration and models files.
+ * It shows ErrorMessageBox when configuration file is not located in disk.
+ * Connects signals to Watchers and add paths to watchers
+ * 
+ **/
 KcmHowdy::KcmHowdy(QWidget *parent, const QVariantList &args) :
     KCModule(parent, args)
 {
@@ -41,16 +48,33 @@ KcmHowdy::KcmHowdy(QWidget *parent, const QVariantList &args) :
     prepareUi();
 }
 
+/**
+ *  Destructor
+ * Nothing to do
+ * 
+ **/
 KcmHowdy::~KcmHowdy()
 {
 
 }
 
+/**
+ * Adds path to mDataWatcher.
+ * It is called when directory is changed, because of unexisting data file.
+ * When data file is removed it looses connection. 
+ * When directory is changed it should mean new file is added and it tries to 
+ * add watcher for file with data of face models.
+ **/
 void KcmHowdy::addPath()
 {
     mDataWatcher.addPath(mModelsFile);
 }
 
+/**
+ *  Function which prepares UI.
+ * It adds ConfigWidget, AddWidget and ModelWidget as tabholders.
+ * 
+ **/
 void KcmHowdy::prepareUi()
 {
     QHBoxLayout* layout = new QHBoxLayout(this);
@@ -72,31 +96,45 @@ void KcmHowdy::prepareUi()
 
 }
 
+/**
+ *  Tells classes ConfigWidget and ModelWidget to save. 
+ *  It is called when button Apply is clicked.
+ * 
+ **/
 void KcmHowdy::save()
 {
     mConfigWidget->save();
     mModelWidget->save();
 }
 
+/**
+ * This function tells ConfigWidget and ModelWidget to load data.
+ * It is called when module is started or when button reset is clicked.
+ *
+ **/
 void KcmHowdy::load()
 {
     mConfigWidget->load();
     mModelWidget->load();
 }
 
+/**
+ * Tells ModelWidget to update table and data in it.
+ * It is called by Watcher of data file.
+ * 
+ **/
 void KcmHowdy::updateTable()
 {
     mModelWidget->updateTable();
 }
 
+/**
+ * Shows message about program.
+ * 
+ **/
 QString KcmHowdy::quickHelp() const
 {
     return "This configuration module allows you to configure Howdy program.";
-}
-
-bool KcmHowdy::apply()
-{
-    return true;
 }
 
 #include "kcmhowdy.moc"

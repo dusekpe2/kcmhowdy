82cc30f83b942fd46d4e4b9f19cbee1476c048ee
Petr Dušek <dusekpe2@fit.cvut.cz>
2019-06-26 19:50:39 +0200

    Update src/facemodel.cpp

--- src/facemodel.cpp
+++ src/facemodel.cpp
@@ -1,6 +1,11 @@
 #include "facemodel.h"
 #include<QDateTime>
 
+/**
+ * Constructor of FaceModel
+ * gets id, time and name in parameters and sets variables.
+ * 
+ **/
 FaceModel::FaceModel(double id, double time, QString name)
 {
     mId = QString::number(id);
@@ -11,22 +16,34 @@ FaceModel::FaceModel(double id, double time, QString name)
 
 }
 
+/**
+ * Getter for id 
+ **/
 QString FaceModel::getId() const
 {
     return mId;
 }
 
+/**
+ * Getter for date
+ **/
 QString FaceModel::getDate() const
 {
     return mDate;
 }
 
+/**
+ * Getter for name
+ **/
 QString FaceModel::getName() const
 {
     return mName;
 }
 
-
+/**
+ * Destructor
+ * Nothing to do
+ **/
 FaceModel::~FaceModel()
 {
 
